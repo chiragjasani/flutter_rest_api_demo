5e80d84eb76a9b04ca9c123660d6bc9f7392a49b
Jasani Chirag <jasani.chirag105@gmail.com>
2022-08-06 17:05:23 +0530

    Flutter_API

---
+++ lib/services/api_service.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+import '../constants.dart';
+import '../model/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      print("response.body : "+ response.body);
+      if (response.statusCode == 200) {
+        List<UserModel> _model = userModelFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
